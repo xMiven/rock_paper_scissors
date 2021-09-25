a90ec88b6ed425a708c51daea6a9f6ac92275e68
Jorge <xmiven@gmail.com>
2021-09-25 10:54:32 +0200

    Added basic structure

---
+++ game.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+    //make the computer return rock, paper or scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+    //compare user input and computer input
+    //return winner
+}
+
+function game() {
+    //loop the game 5 times, keep track of the score
+}
